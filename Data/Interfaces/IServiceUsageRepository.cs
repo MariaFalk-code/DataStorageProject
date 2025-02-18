f40710b181cab5dfd7732ce8e405730f0074a56a
Maria Falk <mariacelf@gmail.com>
2025-02-18 22:00:00 +0100

    Implemented ServiceUseageRepos with method for calculating total cost of project

---
+++ Data/Interfaces/IServiceUsageRepository.cs
@@ -0,0 +1,10 @@
+﻿using Data.Entities;
+
+namespace Data.Interfaces;
+
+public interface IServiceUsageRepository : IBaseRepository<ServiceUsageEntity>
+{
+    Task<IEnumerable<ServiceUsageEntity>> GetServiceUsageByProjectAsync(string projectNumber);
+    Task<decimal> GetTotalCostByProjectAsync(string projectNumber);
+}
+
---
+++ Data/Repositories/ServiceUsageRepository.cs
@@ -0,0 +1,25 @@
+﻿using Data.Context;
+using Data.Entities;
+using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories;
+
+public class ServiceUsageRepository(DataContext context) : BaseRepository<ServiceUsageEntity>(context), IServiceUsageRepository
+{
+
+    public async Task<decimal> GetTotalCostByProjectAsync(string projectNumber)
+    {
+        return await base._context.ServiceUsages
+            .Where(su => su.Project.ProjectNumber == projectNumber)
+            .SumAsync(su => su.Quantity * su.Service.Price);
+
+    }
+    public async Task<IEnumerable<ServiceUsageEntity>> GetServiceUsageByProjectAsync(string projectNumber)
+    {
+        return await base._context.ServiceUsages
+            .Include(su => su.Service)
+            .Where(su => su.Project.ProjectNumber == projectNumber)
+            .ToListAsync();
+    }
+}
