83ef315d0cedec1d94a3221aed01fc6ef7d191e9
Maria Falk <mariacelf@gmail.com>
2025-02-18 18:03:58 +0100

    Implemented ServiceRepository with methods.

---
+++ Data/Repositories/ServiceRepository.cs
@@ -0,0 +1,23 @@
+﻿using Data.Context;
+using Data.Entities;
+using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories;
+
+public class ServiceRepository(DataContext context) : BaseRepository<ServiceEntity>(context), IServiceRepository
+{
+    public async Task<ServiceEntity?> GetServiceWithCustomerAndUsageAsync(int ServiceId)
+    {
+        return await base._context.Services
+            .Include(s => s.ServiceUsages)
+                .ThenInclude(su => su.Project.Customer)
+            .FirstOrDefaultAsync(s => s.Id == ServiceId);
+    }
+    public async Task<IEnumerable<ServiceEntity>> GetAllServicesWithUsageAsync()
+    {
+        return await base._context.Services
+            .Include(s => s.ServiceUsages)
+            .ToListAsync();
+    }
+}
